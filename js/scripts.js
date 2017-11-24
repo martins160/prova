ce69025b7c7b469f4f84a586a0b4febb89e951be
Marcelo Martins <martins160@gmail.com>
2017-11-24 19:21:28 -0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+
+var f = function(){
+    var tabela = document.createElement("table");
+    var x = document.getElementById("linhas").value;
+    var y = document.getElementById("colunas").value;
+    document.body.appendChild(tabela);
+
+    for(var cont = 1; cont <= x; cont++){
+
+        var trx = document.createElement("tr");
+        tabela.appendChild(trx);
+
+
+        for(var cont2 = 1; cont2 <= y; cont2++){
+
+            var tdy = document.createElement("td");
+            var txt = document.createTextNode("Linha" + " " + cont + ", Coluna" + " " + cont2);
+            trx.appendChild(tdy);
+            tdy.appendChild(txt);
+
+        }
+
+    }
+
+}   
